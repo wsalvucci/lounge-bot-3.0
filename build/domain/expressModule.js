f10a1ebcc6dc80c565d229762c4905d459fd5222
wsalvucci <wsalvucci@outlook.com>
2021-06-22 18:59:54 -0400

    build cleanup

--- build/domain/expressModule.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const app = express();
-app.listen(process.env.API_PORT, () => {
-    console.log('API is listening on port ' + process.env.API_PORT);
-});
-exports.default = app;
-//# sourceMappingURL=expressModule.js.map
--- build/models/CommandModule.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CommandModule {
-    constructor(commands) {
-        this.commands = commands;
-    }
-}
-exports.default = CommandModule;
-//# sourceMappingURL=CommandModule.js.map
