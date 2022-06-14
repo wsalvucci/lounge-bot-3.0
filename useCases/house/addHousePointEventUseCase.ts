d0259841e750091b7a3e39571ee29ba6fb1d5baa
wsalvucci <wsalvucci@outlook.com>
2022-06-13 21:35:49 -0400

    Add xp for house points

--- useCases/house/addHousePointEventUseCase.ts
+++ useCases/house/addHousePointEventUseCase.ts
@@ -2,5 +2,6 @@ import loungeApi from "../../api/loungeApi";
 import SqlResponse from "../../responseModels/SqlResponse";
 
 export default function addHousePointEventUseCase(discordId: string, headmasterId: string, points: number, reason: string, houseId: number, timestamp: number, repository: typeof loungeApi): Promise<SqlResponse> {
+    repository.incrementUserProperty(discordId, 'xp', points * 500)
     return repository.addHousePointEvent(discordId, headmasterId, points, reason, houseId, timestamp)
 }
